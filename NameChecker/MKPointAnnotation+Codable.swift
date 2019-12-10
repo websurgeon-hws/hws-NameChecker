3265e44ce02233b7f700e9feed6224d840ff0bc4
Peter Barclay <peter@peter-barclay.com>
2019-12-10 07:47:17 +0000

    fix coding issue for CodableMKPointAnnotation

--- NameChecker/MKPointAnnotation+Codable.swift
+++ NameChecker/MKPointAnnotation+Codable.swift
@@ -19,7 +19,7 @@ public class CodableMKPointAnnotation: MKPointAnnotation, Codable {
 
         let container = try decoder.container(keyedBy: CodingKeys.self)
         title = try container.decode(String.self, forKey: .title)
-        subtitle = try container.decode(String.self, forKey: .subtitle)
+        subtitle = try container.decodeIfPresent(String.self, forKey: .subtitle)
 
         let latitude = try container.decode(CLLocationDegrees.self, forKey: .latitude)
         let longitude = try container.decode(CLLocationDegrees.self, forKey: .longitude)
@@ -29,7 +29,7 @@ public class CodableMKPointAnnotation: MKPointAnnotation, Codable {
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(title, forKey: .title)
-        try container.encode(subtitle, forKey: .subtitle)
+        try container.encodeIfPresent(subtitle, forKey: .subtitle)
         try container.encode(coordinate.latitude, forKey: .latitude)
         try container.encode(coordinate.longitude, forKey: .longitude)
     }
