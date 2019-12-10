e154d4d65b2c24e24913d13593037d363f10b38c
Peter Barclay <peter@peter-barclay.com>
2019-12-10 06:58:27 +0000

    Add MapView & Annotations from BucketList project

---
+++ NameChecker/MKPointAnnotation-ObservableObject.swift
@@ -0,0 +1,27 @@
+//
+//  Copyright © 2019 Peter Barclay. All rights reserved.
+//
+
+import MapKit
+
+extension MKPointAnnotation: ObservableObject {
+    public var wrappedTitle: String {
+        get {
+            self.title ?? "Unknown value"
+        }
+
+        set {
+            title = newValue
+        }
+    }
+
+    public var wrappedSubtitle: String {
+        get {
+            self.subtitle ?? "Unknown value"
+        }
+
+        set {
+            subtitle = newValue
+        }
+    }
+}
