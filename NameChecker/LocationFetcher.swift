08f74c4b1602eb123f4c9a594aed38be8fdf2494
Peter Barclay <peter@peter-barclay.com>
2019-12-10 06:50:52 +0000

    Add LocationFetcher from Paul Hudson

---
+++ NameChecker/LocationFetcher.swift
@@ -0,0 +1,25 @@
+//
+//  Copyright © 2019 Peter Barclay. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationFetcher: NSObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    var lastKnownLocation: CLLocationCoordinate2D?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+
+    func start() {
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        lastKnownLocation = locations.first?.coordinate
+    }
+}
